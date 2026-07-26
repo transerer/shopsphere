bf19d4432c82805c9c5044d360c92423f312d3e5
Satish Singh <sattish1996@yahoo.in>
2026-07-26 18:27:34 +0530

    Initial commit

---
+++ gateway/src/main/java/org/shopshere/gateway/filter/RequestLoggingFilter.java
@@ -0,0 +1,39 @@
+package org.shopshere.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@Slf4j
+public class RequestLoggingFilter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(
+            ServerWebExchange exchange,
+            GatewayFilterChain chain) {
+
+
+        String path = exchange.getRequest()
+                .getPath()
+                .value();
+
+
+        log.info("Incoming request path={}", path);
+
+
+        return chain.filter(exchange);
+    }
+
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
---
+++ user-service/src/main/java/org/shopshere/userservice/UserController.java
@@ -0,0 +1,15 @@
+package org.shopshere.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from User Service";
+    }
+}
